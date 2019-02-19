73470e2a47362eba7bf32aa41cca81a79aff262b
yhbao666@gmail.com <yhbao666@gmail.com>
2019-02-19 20:40:39 +0800

    docs

---
+++ Assets/Script/AllInit.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+
+
+public class AllInit : MonoBehaviour {
+    private bool bFirst = true;
+    public double r, R, h;
+    public int totolPlate = 4;
+    public int rMax;
+    public int rMin;
+    private bool drag = false;
+    GameObject selectedRing = null;
+    readonly int Left = 0;
+    readonly int Mid = 1;
+    readonly int Right = 2;
+    private Stack<GameObject>[] gameObjectsStack = new Stack<GameObject>[] {new Stack<GameObject>(),new Stack<GameObject>(),new Stack<GameObject>()};
+
+    private struct PillarStruct
+    {
+       public Vector3 postion;
+       public float h;
+    }
+
+    private List<PillarStruct> GetPillarPos()
+    {
+        var PillarAPosAndH = GetPillarPosition("Pillars/PillarA");
+        var PillarBPosAndH = GetPillarPosition("Pillars/PillarB");
+        var PillarCPosAndH = GetPillarPosition("Pillars/PillarC");
+        List<PillarStruct> PillarPosTmp = new List<PillarStruct>();
+        PillarPosTmp.Add(PillarAPosAndH);
+        PillarPosTmp.Add(PillarBPosAndH);
+        PillarPosTmp.Add(PillarCPosAndH);
+        return PillarPosTmp;
+    }
+
+    private PillarStruct GetPillarPosition (string url)
+    {
+        var obj = GameObject.Find(url);
+        if (obj == null)
+            return new PillarStruct();
+        PillarStruct tmp = new PillarStruct();
+        tmp.postion = obj.transform.position;
+        tmp.h = (float)obj.GetComponent<Ring>().h;
+        return tmp;
+    }
+
+
+    List<PillarStruct> PillarPos = new List<PillarStruct>();
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (totolPlate < 0)
+        {
+            return;
+        }
+        if (bFirst)
+        {
+            var CylinderPlaneSample = GameObject.Find("CylinderPlaneSample");
+            Stack<GameObject> tmpStack = new Stack<GameObject>();
+            if (CylinderPlaneSample == null)
+            {
+                return;
+            }
+
+            var stepR = (float)(CylinderPlaneSample.GetComponent<Collider>().bounds.size.x - 0.5f * CylinderPlaneSample.GetComponent<Collider>().bounds.size.x) / (totolPlate - 1);
+            var stepH = 2.0f * CylinderPlaneSample.GetComponent<Collider>().bounds.size.y;
+            (CylinderPlaneSample.GetComponent<AddAttribute>() as AddAttribute).ID = totolPlate;
+            (CylinderPlaneSample.GetComponent<AddAttribute>() as AddAttribute).whichDir = Mid;
+            PillarPos = GetPillarPos();
+
+            var accumulation = 0f;
+            var accumulationX = 1f;
+            var rationR = Mathf.Pow(0.3f,1f/ (totolPlate-1));
+            
+            for (int i = totolPlate; i > 0 ; i--)
+            {
+                accumulationX *= rationR;
+                //Debug.Log(rationR);
+                accumulation = accumulation + stepH;
+                var CylinderPlaneSingleOne = GameObject.Instantiate(CylinderPlaneSample);
+                CylinderPlaneSingleOne.transform.position = CylinderPlaneSample.transform.position + new Vector3(0, accumulation, 0);
+                CylinderPlaneSingleOne.transform.localScale =
+                    new Vector3(CylinderPlaneSample.transform.localScale.x * accumulationX,
+                    CylinderPlaneSample.transform.localScale.y,
+                    CylinderPlaneSample.transform.localScale.z * accumulationX);
+                (CylinderPlaneSingleOne.GetComponent<AddAttribute>() as AddAttribute).ID = i;
+                (CylinderPlaneSingleOne.GetComponent<AddAttribute>() as AddAttribute).whichDir = Mid;
+                tmpStack.Push(CylinderPlaneSingleOne);
+            }
+            gameObjectsStack[Mid] = tmpStack;
+            
+            CylinderPlaneSample.SetActive(false);
+            bFirst = false;
+        }
+
+        if (Input.GetMouseButton(0) & !drag)//left click
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  //camare2D.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag == "Ring")
+                {
+                     if((hit.collider.gameObject.GetComponent<AddAttribute>() as AddAttribute).ID == (gameObjectsStack[(hit.collider.gameObject.GetComponent<AddAttribute>() as AddAttribute).whichDir].Peek().GetComponent<AddAttribute>() as AddAttribute).ID)
+                    {
+                        selectedRing = hit.collider.gameObject;
+                        gameObjectsStack[(hit.collider.gameObject.GetComponent<AddAttribute>() as AddAttribute).whichDir].Pop();
+                        drag = true;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(1) & drag)//right click => DragOff
+        {
+            if(selectedRing & FuncDragOff())
+            {
+                drag = false;
+            }
+            
+        }
+
+        if (drag & selectedRing) //Drag
+        {
+            Vector3 Pos = Camera.main.WorldToScreenPoint(selectedRing.transform.position);// 获取目标对象当前的世界坐标系位置，并将其转换为屏幕坐标系的点
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Pos.z);// 设置鼠标的屏幕坐标向量，用上面获得的Pos的z轴数据作为鼠标的z轴数据，使鼠标坐标与目标对象坐标处于同一层面上
+            selectedRing.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+    }
+
+    private bool FuncDragOff() {
+        float[] arraytmp = new float[]{
+                Vector3.Distance(selectedRing.transform.position, PillarPos[Left].postion),
+                Vector3.Distance(selectedRing.transform.position, PillarPos[Mid].postion),
+                Vector3.Distance(selectedRing.transform.position, PillarPos[Right].postion)
+                };
+        int selectedWhich = Array.IndexOf(arraytmp, arraytmp.Min());
+        
+        selectedRing.transform.position = new Vector3(
+            PillarPos[selectedWhich].postion.x,
+            PillarPos[selectedWhich].h,
+            PillarPos[selectedWhich].postion.z
+        );
+
+        int idstate;
+        if (gameObjectsStack[selectedWhich].Count == 0)
+            idstate = int.MaxValue;
+        else
+            idstate = (gameObjectsStack[selectedWhich].Peek().GetComponent<AddAttribute>() as AddAttribute).ID;
+
+       int  selectedRingID = (selectedRing.GetComponent<AddAttribute>() as AddAttribute).ID;
+
+        if(idstate > selectedRingID)
+        {
+            (selectedRing.GetComponent<AddAttribute>() as AddAttribute).whichDir = selectedWhich;
+            gameObjectsStack[selectedWhich].Push(selectedRing);
+            return true;
+        }
+        return false;
+
+
+    }
+
+    
+}
---
+++ Assets/Script/Ring.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Ring : MonoBehaviour {
+    public double r, R, h;
+    public string dirction;
+    private int startangle = 0, endangle = 360;
+    private bool bFirst = true;
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (bFirst)
+        {
+            var PillarsB = GameObject.Find("Pillars/PillarB");
+            if (PillarsB == null)
+            {
+                return;
+            }
+
+            var GameObjectItem = GameObject.Find("GameObject").GetComponent<AllInit>() as AllInit;
+            h = GameObjectItem.totolPlate * 0.2;
+            GetRing();
+            var offPositionCylinder = new Vector3(0, 0, 0);
+            if (dirction == "Left")
+            {
+                offPositionCylinder = new Vector3(5, 0, 0);
+            }
+            else if (dirction == "Right")
+            {
+                offPositionCylinder = new Vector3(-5, 0, 0);
+            }
+            else if (dirction == "Mid")
+            {
+                offPositionCylinder = new Vector3(0, 0, 0);
+            }
+            gameObject.transform.position = PillarsB.transform.position - offPositionCylinder;
+            bFirst = false;
+        }
+	}
+
+    public void GetRing()
+    {
+        if (r < 0 | R < 0 | h < 0)
+        {
+            return;
+        }
+
+        List<Vector3> verticesallList = null;
+        List<int> trianglesallList = null;
+        GetInOutUpDown(ref verticesallList, ref trianglesallList);
+
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = mf.mesh;
+        mesh.Clear();
+        mesh.vertices = verticesallList.ToArray();
+        mesh.triangles = trianglesallList.ToArray();
+        verticesallList.Clear();
+        trianglesallList.Clear();
+    }
+   
+    private void GetInOutUpDown(ref List<Vector3> verticesList, ref List<int> trianglesList)
+    {
+        int bins = 36;
+
+        List<Vector3> verticesrList = new List<Vector3>();
+        List<int> trianglesrList = new List<int>();
+        GetVerticesAndTriangles(ref verticesrList, ref trianglesrList, bins, startangle, endangle, r);//内环
+
+        List<Vector3> verticesRList = new List<Vector3>();
+        List<int> trianglesRList = new List<int>();
+        GetVerticesAndTriangles(ref verticesRList, ref trianglesRList, bins, startangle, endangle, R);//外环
+
+        //整理内外环点
+        verticesList = new List<Vector3>();
+        verticesList.AddRange(verticesrList);
+        verticesList.AddRange(verticesRList);
+
+        //整理内外环三角形
+        trianglesList = new List<int>();
+        trianglesList.AddRange(trianglesrList);
+        for (int i = 0; i < trianglesRList.Count; i++)
+        {
+            trianglesRList[i] = trianglesRList[i] + verticesrList.Count;
+        }
+        trianglesList.AddRange(trianglesRList);
+
+        //整理底面三角形
+        for (int i = 0; i < bins; i++)
+        {
+            if (i == bins - 1)
+                break;
+            int[] triangletmp = new int[] {
+             i + 2 * bins,
+            i,
+            (i + 1) % bins,
+
+              i + 2 * bins,
+            (i + 1) % bins,
+            i,
+
+
+            (i + 1) % bins,
+            (i + 1) % bins + 2 * bins,
+            i + (2 * bins),
+
+            (i + 1) % bins,
+            i + (2 * bins),
+            (i + 1) % bins + 2 * bins,
+            };
+            trianglesList.AddRange(triangletmp);
+        }
+       
+        //整理顶面三角形
+        for (int i = 0; i < bins; i++)
+        {
+            if (i == bins - 1)
+                break;
+            int[] triangletmp = new int[] {
+            i + 3 * bins,
+            i + bins,
+            (i + 1) % bins + 3 * bins,
+
+            i + bins,
+            i + 3 * bins,
+            (i + 1) % bins + 3 * bins,
+
+            i + bins,
+            (i + 1) % bins + bins,
+            (i + 1) % bins + 3 * bins,
+
+            (i + 1) % bins + bins,
+            i + bins,
+            (i + 1) % bins + 3 * bins,
+        };
+            trianglesList.AddRange(triangletmp);
+        }
+    }
+    private void GetVerticesAndTriangles(ref List<Vector3> verticesList, ref List<int> trianglesList, int bins = 10, int startangle = 0, int endangle = 360, double r = 1)
+    {
+        Vector3[] vertices = new Vector3[2 * bins];
+        int[] trianglesrsideonepositive = new int[3 * (bins - 1)];
+        int[] trianglesrsidetwopositive = new int[3 * (bins - 1)];
+        int[] trianglesrsideonenegative = new int[3 * (bins - 1)];
+        int[] trianglesrsidetwonegative = new int[3 * (bins - 1)];
+
+        for (int i = 0; i < bins; i++)
+        {
+            var x = (float)(Mathf.Cos( (Mathf.PI /180) * (endangle - startangle)  / (bins - 1) * i + (Mathf.PI / 180) * startangle) * r);
+            var y = (float)h;
+            var z = (float)(Mathf.Sin( (Mathf.PI / 180) * (endangle - startangle) / (bins -1) * i + (Mathf.PI / 180) * startangle) * r);
+
+            vertices[i] = new Vector3(x, 0, z);///点
+            vertices[i + bins] = new Vector3(x, y, z);
+
+            if (i == bins - 1)
+            {
+                break;
+            }
+            int[] trianglesrtmp = new int[] {
+                                                i, i + bins, (i + 1) % bins,///顺时针
+
+                                                i + bins, i, (i + 1) % bins,///逆时针
+
+                                                i + bins, (i + 1) % bins + bins, (i + 1) % bins,///顺时针
+
+                                                i + bins, (i + 1) % bins, (i + 1) % bins + bins,///逆时针
+            };
+            trianglesList.AddRange(trianglesrtmp);
+        }
+        verticesList.AddRange(vertices);
+    }
+
+}
